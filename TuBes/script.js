e6e0921ae17eb3458d06ad2c07a46ec209716582
rephrain <rephrain1@gmail.com>
2022-05-30 20:10:22 +0700

    [NEW] KelolaUser.html+script.js+KelolaUser.css

--- TuBes/script.js
+++ TuBes/script.js
@@ -134,3 +134,16 @@ function off2() {
 document.getElementById("sign_out").addEventListener("onclick", () => {
     document.location = 'Login Page.html';
 })
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
