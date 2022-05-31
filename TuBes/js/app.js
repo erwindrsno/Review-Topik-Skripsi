d2011c5f52beb67d842566c05eb2df0b8e7200ac
Erwin Darsono <erwindarsono08@gmail.com>
2022-05-31 11:37:58 +0800

    [UPDATE] rendering url berhasi, tapi CSS tidak tertampilkan

--- TuBes/js/app.js
+++ TuBes/js/app.js
@@ -1,14 +1,12 @@
 import http from 'http';
 import fs from 'fs';
+import express from 'express';
 
 const port = 8080;
 
-const server = http.createServer((req,res) => {
-    const url = req.url;
-    console.log(url);
-    
-    fs.readFile('../Login Page.html', (err,data) => {
-        if(err){
+const renderHTML = (path,res) => {
+    fs.readFile(path, (err,data) => {
+        if(err) {
             res.writeHead(404);
             res.write('Error: file not found');
         }
@@ -17,6 +15,28 @@ const server = http.createServer((req,res) => {
         }
         res.end();
     })
+}
+
+const server = http.createServer((req,res) => {
+    const url = req.url;
+    console.log(url);
+    switch(url){
+        case '/kelola-user':
+            renderHTML('../kelola-user.html',res);
+            break;
+
+        case '/halaman-review':
+            renderHTML('../halaman-review.html',res);
+            break;
+
+        case '/unggah-topik':
+            renderHTML('../unggah-topik.html',res);
+            break;
+
+        default:
+            renderHTML('../login-page.html',res);
+            break;
+    }
 })
 
 server.listen(port, () => {
