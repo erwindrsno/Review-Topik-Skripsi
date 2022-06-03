7037e92fcbb383bb9e23cbb2021862cc27d1739d
Erwin Darsono <erwindarsono08@gmail.com>
2022-06-03 22:00:11 +0800

    [UPDATE] sign in sudah bisa dapat input

--- TuBes/app.js
+++ TuBes/app.js
@@ -14,59 +14,19 @@ app.set('view engine','ejs');
 const staticPath = path.resolve('public');
 app.use(express.static(staticPath));    //serving static page dari public
 
+app.use(express.urlencoded({ extended: true})); //?
 
 app.get('/', (req,res) => {
-    // res.send('Hello world!');
     res.sendFile('/index.html');
 });
 
-app.post('/signup', multerParser.none(), (req,res) => {
-    console.log(req.body)
-})
+app.post('/signin', multerParser.none(), (req,res) => {
+    console.log(req.body);
+});
 
 app.get('/halaman-review', (req,res) => {
     res.sendFile('/halaman-review.html');
-})
-
-app.use('/', (req,res) => {
-    res.send('Halaman tidak tersedia!');
-    res.statusCode(404);
 });
-
-// const renderHTML = (path,res) => {
-//     fs.readFile(path, (err,data) => {
-//         if(err) {
-//             res.writeHead(404);
-//             res.write('Error: file not found');
-//         }
-//         else{
-//             res.write(data);
-//         }
-//         res.end();
-//     })
-// }
-
-// const server = http.createServer((req,res) => {
-//     const url = req.url;
-//     console.log(url);
-//     switch(url){
-//         case '/kelola-user':
-//             renderHTML('./kelola-user.html',res);
-//             break;
-
-//         case '/halaman-review':
-//             renderHTML('./halaman-review.html',res);
-//             break;
-
-//         case '/unggah-topik':
-//             renderHTML('./unggah-topik.html',res);
-//             break;
-
-//         default:
-//             renderHTML('./login-page.html',res);
-//             break;
-//     }
-// })
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
