5d63fa4d157fb0586fd168bb9ee68481d4a0ecf9
rephrain <rephrain1@gmail.com>
2022-06-26 02:10:01 +0700

    [NEW] convert to pdf

--- TuBes/public/script/script.js
+++ TuBes/public/script/script.js
@@ -320,3 +320,49 @@ function encodeURL(data){
     return ret.join('&');
 }
 // fetch('signin')
+function printToPDF() {
+    console.log('converting...');
+  
+    var printableArea = document.getElementById('pdf');
+  
+    html2canvas(printableArea, {
+      useCORS: true,
+      onrendered: function(canvas) {
+  
+        var pdf = new jsPDF('p', 'pt', 'letter');
+  
+        var pageHeight = 980;
+        var pageWidth = 2500;
+
+        for (var i = 0; i <= printableArea.clientHeight / pageHeight; i++) {
+          var srcImg = canvas;
+          var sX = 0;
+          var sY = pageHeight * i; 
+          var sWidth = pageWidth;
+          var sHeight = pageHeight;
+          var dX = 0;
+          var dY = 0;
+          var dWidth = pageWidth;
+          var dHeight = pageHeight;
+  
+          window.onePageCanvas = document.createElement("canvas");
+          onePageCanvas.setAttribute('width', pageWidth);
+          onePageCanvas.setAttribute('height', pageHeight);
+          var ctx = onePageCanvas.getContext('2d');
+          ctx.drawImage(srcImg, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight);
+  
+          var canvasDataURL = onePageCanvas.toDataURL("image/png", 1.0);
+          var width = onePageCanvas.width;
+          var height = onePageCanvas.clientHeight;
+  
+          if (i > 0) 
+            pdf.addPage(612, 791);
+  
+          pdf.setPage(i + 1); 
+          pdf.addImage(canvasDataURL, 'PNG', 28, 40, (width * .65), (height * .62));
+  
+        }
+        pdf.save('DaftarTopik.pdf');
+      }
+    });
+  }
